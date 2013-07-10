ee6525d722efb834bb0765c77b460355bfa79733
John Smith <ubuntu@ip-10-147-139-248.ec2.internal>
2013-07-10 13:46:52 +0000

    my first website

--- web.js
+++ web.js
@@ -1,20 +1,14 @@
+var fs = require('fs');
 var express = require('express');
-var fs = require( 'fs');
 
 var app = express.createServer(express.logger());
-var buf_contents = fs.readFileSync('index.html','utf-8');
-var str_contents = buf_contents.toString();
-
 
 app.get('/', function(request, response) {
-<<<<<<< HEAD
-  response.send(str_contents);});
-=======
-  response.send(str_contents);
+    var buffer = fs.readFileSync('index.html');
+    response.send(buffer.toString('utf-8'));
 });
->>>>>>> 77ec652dab9129e07f2e8a757ff01ee21003121c
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
