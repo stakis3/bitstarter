3b389b4c920a49cb2a3819e3402b265b4ede5553
John Smith <ubuntu@ip-10-147-139-248.ec2.internal>
2013-07-10 10:01:33 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require( 'fs')
 
 var app = express.createServer(express.logger());
+var buf_contents = fs.readFileSync('index.html','utf-8');
+var str_contents = buf_contents.toString();
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('str_contents');
 });
 
 var port = process.env.PORT || 5000;
